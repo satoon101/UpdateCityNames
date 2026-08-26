9aa2c8f06dd180a47390d6e0cbcecb3bc78f601f
satoon101 <satoon101@gmail.com>
2026-08-25 22:53:25 -0400

    Removed storing the data for save/load in favor of just retrieving the necessary data on load.

--- scripts/Gameplay/UpdateCityNames.lua
+++ scripts/Gameplay/UpdateCityNames.lua
@@ -5,65 +5,30 @@
 
 print("=== Update City Names (Gameplay) Loading ===")
 
-SAVE_KEY_CURRENT_CITY_NAMES = "UpdateCityNames_CurrentCityPlots"
-g_CurrentCityNames = {}
+local UPDATE_IS_ENABLED = false
+CurrentCityNames = {}
 
-function storeCurrentCityNames()
-    local serializedString = ""
-
-    for plotKey, isExempt in pairs(g_CurrentCityNames) do
-        if isExempt then
-            if string.len(serializedString) > 0 then
-                serializedString = serializedString .. ";" .. plotKey
-            else
-                serializedString = plotKey
-            end
-        end
-    end
-
-    -- Write the raw string data into the game engine's save configuration file
-    Game:SetProperty(SAVE_KEY_CURRENT_CITY_NAMES, serializedString)
-end
-
-function loadCurrentCityNames()
-    g_CurrentCityNames = {} -- Reset memory allocation cleanly
-
-    -- Retrieve the saved string data from the loaded file structure
-    local serializedString = Game:GetProperty(SAVE_KEY_CURRENT_CITY_NAMES)
-
-    if serializedString ~= nil then
-        if string.len(serializedString) > 0 then
-            -- Split the string by our semicolon delimiter
-            for plotKey in string.gmatch(serializedString, "([^;]+)") do
-                g_CurrentCityNames[plotKey] = true
-            end
-        end
-    else
-        findCurrentCityNames()
-    end
-end
-
-function findCurrentCityNames()
+local function FindCurrentCityNames()
     for _, playerID in ipairs(PlayerManager.GetAliveIDs()) do
         local player = Players[playerID]
         if player ~= nil then
             for _, city in player:GetCities():Members() do
                 local plotKey = city:GetX() .. "," .. city:GetY()
-                g_CurrentCityNames[plotKey] = city:GetName()
+                CurrentCityNames[plotKey] = city:GetName()
             end
         end
     end
 end
 
-function OnTurnBegin()
-    findCurrentCityNames()
-    storeCurrentCityNames()
-end
+Events.LoadScreenClose.Add(FindCurrentCityNames)
+Events.TurnBegin.Add(FindCurrentCityNames)
 
-Events.TurnBegin.Add(OnTurnBegin)
+local function UpdateCityName(playerID, cityID)
+    if not UPDATE_IS_ENABLED then
+        return
+    end
 
-function updateCityName(playerID, cityID)
-    if next(g_CityNames) == nil then
+    if next(CityNames) == nil then
         print("Array is empty.")
         return
     end
@@ -78,13 +43,13 @@ function updateCityName(playerID, cityID)
     local cities = player:GetCities()
     local city = cities:FindID(cityID)
     local plotKey = city:GetX() .. "," .. city:GetY()
-    if g_CurrentCityNames[plotKey] then
+    if CurrentCityNames[plotKey] then
         print("Not updating name")
         return
     end
 
     local city_count = cities:GetCount()
-    local city_name = g_CityNames[city_count]
+    local city_name = CityNames[city_count]
     if city_name == nil then
         print("No city name found.")
         return
@@ -98,23 +63,24 @@ function updateCityName(playerID, cityID)
 
     print("Setting name.  Current: " .. current_name .. " New: " .. city_name)
     city:SetName(city_name)
+    CurrentCityNames[plotKey] = city_name
 end
 
-Events.CityAddedToMap.Add(updateCityName)
+Events.CityAddedToMap.Add(UpdateCityName)
 
-g_CityNames = {}
-SAN_FRANCISCO_CITY_NUMBER = 47
-SAN_FRANCISCO_CITY_NAME = "LOC_CITY_NAME_SAN_FRANCISCO"
-HOUSTON_CITY_NUMBER = 55
-HOUSTON_CITY_NAME = "LOC_CITY_NAME_HOUSTON"
-DEFAULT_CITY_NAMES = {
-    [SAN_FRANCISCO_CITY_NUMBER] = SAN_FRANCISCO_CITY_NAME,
-    [HOUSTON_CITY_NUMBER] = HOUSTON_CITY_NAME,
+CityNames = {}
+KNOWN_CITY_NAMES_BY_COUNT = {
+    [47] = "LOC_CITY_NAME_SAN_FRANCISCO",
+    [55] = "LOC_CITY_NAME_HOUSTON",
 }
+KNOWN_COUNT_BY_CITY_NAMES = {}
+for k, v in pairs(KNOWN_CITY_NAMES_BY_COUNT) do
+    KNOWN_COUNT_BY_CITY_NAMES[v] = k
+end
 
-function gatherCityNames()
-    loadCurrentCityNames()
-    if next(g_CityNames) ~= nil then
+local function GatherCityNames()
+    UPDATE_IS_ENABLED = true
+    if next(CityNames) ~= nil then
         return
     end
 
@@ -131,39 +97,32 @@ function gatherCityNames()
 
     local count = 1
     for row in GameInfo.CityNames() do
-        if (
-            count == SAN_FRANCISCO_CITY_NUMBER or
-            count == HOUSTON_CITY_NUMBER
-        ) then
-            g_CityNames[count] = DEFAULT_CITY_NAMES[count]
+        local knownName = KNOWN_CITY_NAMES_BY_COUNT[count]
+        if knownName ~= nil then
+            CityNames[count] = knownName
             count = count + 1
         end
         if row.CivilizationType == civName then
-            g_CityNames[count] = row.CityName
+            CityNames[count] = row.CityName
             count = count + 1
         end
     end
 
     for row in GameInfo.CityNames() do
-        if (
-            count == SAN_FRANCISCO_CITY_NUMBER or
-            count == HOUSTON_CITY_NUMBER
-        ) then
-            g_CityNames[count] = DEFAULT_CITY_NAMES[count]
+        local knownName = KNOWN_CITY_NAMES_BY_COUNT[count]
+        if knownName ~= nil then
+            CityNames[count] = knownName
             count = count + 1
         end
         if row.CivilizationType == "CIVILIZATION_AMERICA" then
-            if (
-                row.CityName ~= SAN_FRANCISCO_CITY_NAME and
-                row.CityName ~= HOUSTON_CITY_NAME
-            ) then
-                g_CityNames[count] = row.CityName
+            if KNOWN_COUNT_BY_CITY_NAMES[row.CityName] == nil then
+                CityNames[count] = row.CityName
                 count = count + 1
             end
         end
     end
 end
 
-Events.LoadScreenClose.Add(gatherCityNames)
+Events.LoadScreenClose.Add(GatherCityNames)
 
 print("=== Update City Names (Gameplay) Loaded ===")
